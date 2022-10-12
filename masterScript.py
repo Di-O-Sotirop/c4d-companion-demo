f13d43ee7c4bdbebfc673a9e368edfb287b3cb34
Di-O-Sotirop <115657455+Di-O-Sotirop@users.noreply.github.com>
2022-10-12 20:45:37 +0200

    Add files via upload

--- masterScript.py
+++ masterScript.py
@@ -14,14 +14,14 @@ import time, cv2, datetime
 import numpy as np
 import onnxruntime as ort
 
-from c4d_sw.c4dCnnPkg import ResizeAndPad, FilterBoxesNMS, inputPreprocess, outputPreprocess, printBBoxes, \
-    WriteC4DVideo, PrintBBoxOnFrame
-from c4d_sw.pymavlinkPkg import initialize_mavlink, PX4setMode
+from c4d_sw import c4dCnnPkg as c4dcnn
+from c4d_sw import pymavlinkPkg as mavl
+from c4d_sw import c4dSettings as sett
+from c4d_sw import aesHelperPkg as aesh
 
 # import subprocess
 import subprocess
-import c4d_sw.c4dSettings
-from c4d_sw.aesHelperPkg import formatNumeric, formatPlantCnt
+
 
 ################################################################################################
 # Settings
@@ -31,7 +31,7 @@ from c4d_sw.aesHelperPkg import formatNumeric, formatPlantCnt
 ################################################################################################
 # Init
 ################################################################################################
-vehicle = initialize_mavlink(connection_string='/dev/ttyACM0')
+vehicle = mavl.initialize_mavlink(connection_string='/dev/ttyACM0')
 
 ## Listener ##
 home_position_set = False
@@ -52,15 +52,15 @@ inputSize = (640, 640)
 opts = ort.SessionOptions()
 opts.intra_op_num_threads = 4
 opts.inter_op_num_threads = 1
-session = ort.InferenceSession(c4d_sw.model, sess_options=opts)
+session = ort.InferenceSession(c4dcnn.model, sess_options=opts)
 input_name = session.get_inputs()[0].name
 output_0_name = session.get_outputs()[0].name
 
 # Import and Split Video
-if c4d_sw.camera == False:
-    cap = cv2.VideoCapture(c4d_sw.vid_path)
+if sett.camera == False:
+    cap = cv2.VideoCapture(sett.vid_path)
 else:
-    cap = cv2.VideoCapture(c4d_sw.cameraID)
+    cap = cv2.VideoCapture(sett.cameraID)
 frm_count = 0
 
 # Read cap once to get constants
@@ -87,7 +87,7 @@ if cap.isOpened():
     print(" Lon: %s" % vehicle.location.global_relative_frame.lon)
     print(" Lat: %s" % vehicle.location.global_relative_frame.lat)
     # Change to AUTO mode
-    PX4setMode(c4d_sw.MAV_MODE_AUTO)
+    mavl.PX4setMode(mavl.MAV_MODE_AUTO)
     time.sleep(1)
     # get global position
     home = vehicle.location.global_relative_frame
@@ -107,10 +107,12 @@ while (cap.isOpened()):
     longitude = vehicle.location.global_relative_frame.lon
     altitude = vehicle.location.global_relative_frame.alt
 
-    outMSG = outMSG + formatNumeric(latitude, 2, 10) + formatNumeric(longitude, 2, 10) + formatNumeric(altitude, 2, 10)
+    outMSG = outMSG + aesh.formatNumeric(latitude, 2, 10) + aesh.formatNumeric(longitude, 2, 10) + aesh.formatNumeric(
+        altitude, 2, 10)
     # Capture Speed
-    outMSG = outMSG + formatNumeric(str(vehicle.velocity.vx), 2, 7) + formatNumeric(str(vehicle.velocity.vy), 2,
-                                                                                    7) + formatNumeric(
+    outMSG = outMSG + aesh.formatNumeric(str(vehicle.velocity.vx), 2, 7) + aesh.formatNumeric(str(vehicle.velocity.vy),
+                                                                                              2,
+                                                                                              7) + aesh.formatNumeric(
         str(vehicle.velocity.vz), 2, 7)
 
     #  LonLatAlt = []
@@ -124,14 +126,14 @@ while (cap.isOpened()):
         break
 
     # Image Pre-Processing
-    data = inputPreprocess(frame)
+    data = c4dcnn.inputPreprocess(frame, (640, 640))
     ###########################INFERENCE_WITH_ONNXRUNTIME###################################
     [result_0] = session.run([output_0_name], {input_name: data})
     #############################PREPROCESS OUTPUT############################################
     mOutputRow = session.get_outputs()[0].shape[1]  # 25200
     mOutputColumn = session.get_outputs()[0].shape[2]  # 6
 
-    boxes = np.array(outputPreprocess(result_0, mOutputRow, mOutputColumn, frame))
+    boxes = np.array(c4dcnn.outputPreprocess(result_0, mOutputRow, mOutputColumn, frame))
 
     # Sort Boxes for NMS
     if boxes.shape[0] > 1:
@@ -139,19 +141,19 @@ while (cap.isOpened()):
     else:
         boxes_sorted = boxes
     # apply NMS
-    rem_bbox = FilterBoxesNMS(boxes, c4d_sw.thresh)
+    rem_bbox = c4dcnn.FilterBoxesNMS(boxes, sett.thresh)
     rem_bbox = np.array(rem_bbox)
 
     # Add artichoke count to out Msg
-    outMSG = outMSG + formatPlantCnt(rem_bbox.shape[0], 3)
+    outMSG = outMSG + c4dcnn.formatPlantCnt(rem_bbox.shape[0], 3)
 
     # Print Count / BBoxes on Frame
-    if c4d_sw.artichokeCount == True:
+    if sett.artichokeCount == True:
         print(rem_bbox.shape[0])
-    if c4d_sw.boxOnFrame == True:
+    if sett.boxOnFrame == True:
         # Print Boxes on frame and write frames
-        (img_array, frm_count) = PrintBBoxOnFrame(frame, rem_bbox, frm_count, img_array)
-        if not c4d_sw.full_video:
+        (img_array, frm_count) = c4dcnn.PrintBBoxOnFrame(frame, rem_bbox, frm_count, img_array)
+        if not sett.full_video:
             if frm_count > num_of_frames:
                 break
         else:
@@ -165,8 +167,8 @@ cap.release()
 cv2.destroyAllWindows()
 
 ##  Write Video avi ##
-if c4d_sw.write_video == True:
-    WriteC4DVideo(img_array)
+if sett.write_video == True:
+    c4dcnn.WriteC4DVideo(img_array)
 
 # Close vehicle object before exiting script
 vehicle.close()
